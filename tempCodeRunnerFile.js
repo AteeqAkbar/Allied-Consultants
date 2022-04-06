69047c4647945413212afe3b611e5a5f368211e8
AteeqAkbar <attiqdoger3@gmail.com>
2022-04-06 15:03:12 +0500

    PROMISE EXAMPE

--- tempCodeRunnerFile.js
+++ tempCodeRunnerFile.js
@@ -1,1 +1,20 @@
-Fulfill
+
+    if (a <= 10) {
+        res(a);
+    } else {
+
+        rej(a);
+    }
+}
+
+function num(a) {
+    return new Promise(f)
+
+}
+num(2).then((result) => {
+    console.log(`Number ${result} is less then 10`);
+
+}).catch((err) => {
+    console.log(`Number ${err} is greater then 10`);
+
+});
